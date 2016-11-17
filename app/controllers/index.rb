6bd7a4f27eb6bb6dd93937cc84c88ffa2e60d2ba
ZSoJes <GomezSoteloJuan@Gmail.com>
2016-11-17 17:20:53 -0600

    solucionando errores menores

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,7 +7,11 @@ end
 post '/fetch' do
   puts "Publicar un nuevo tweet..."
   @tweet = params[:mensaje]
-  unless @tweet == ""
+  puts "::::::::::::::::::::::::::::::::::::"
+  puts @tweet.nil?
+  puts @tweet.blank?
+  puts "::::::::::::::::::::::::::::::::::::"
+  unless @tweet.blank?
     CLIENT.update(@tweet)
   end
 
@@ -27,25 +31,26 @@ end
 # end
 
 get '/:username' do
-  user = params[:username]
+  @user = params[:username]
 
-  tuit_user = TwitterUser.find_or_create_by(name_user: user)
-  tuit_log = Tweet.where(id: tuit_user.id)
-  user_data = CLIENT.user_search(user).first
+  tuit_user = TwitterUser.find_or_create_by(name_user: @user)
+  tuit_log = Tweet.where(id: tuit_user.id)                                 # busca los twits del este usuario en bd
+  user_data = CLIENT.user_search(@user).first                              # busca en API todo de usuario
 
-  @full_name = user_data.name
-  @url = user_data.profile_image_url("original")
+  @full_name = user_data.name                                              # nombre
+  @url = user_data.profile_image_url("original")                           # avatar
 
-   @tweets_c = CLIENT.user_timeline(user_data.user_name)
+  @tweets_c = CLIENT.user_timeline(user_data.user_name)
 
-   if tuit_log.empty?                   # La base de datos no tiene tweets?  
-    @tweets_c.each do  |t|
+  if tuit_log.empty?                                                       # La base de datos no tiene tweets?
+    @tweets_c.reverse_each do  |t|
       Tweet.create(twitter_user_id: user_data.id, tweet_w: t.text)
     end
   end
 
-  if Time.now - @tweets_c.first.created_at > 700
-     @tweets_c.each do  |t|
+  @tiempo = Time.now - @tweets_c.last.created_at                           #desde el ultimo tuit
+  if Time.now - @tweets_c.last.created_at > 5000
+     @tweets_c.reverse_each do  |t|
        if Tweet.find_by(tweet_w: t.text).nil?
          Tweet.create(twitter_user_id: user_data.id, tweet_w: t.text)
        end
@@ -54,6 +59,6 @@ get '/:username' do
 
 
   # Se hace una petición por los ultimos 10 tweets a la base de datos. 
-  @tweets = Tweet.where(twitter_user_id: user_data.id).order(:created_at).limit(10)
+  @tweets = Tweet.where(twitter_user_id: user_data.id).order(:created_at).last(10)
   erb :twitter_handle
 end
